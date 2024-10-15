46cc052a6dceffb92a6770b7088122bdd1a48566
Kimyechan2154 <kimyechan2959@gmail.com>
2024-10-15 19:58:21 +0900

    Add files via upload

---
+++ cout cin.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    
+    string my_name;
+    cout << "나의 나이는?" <<std::endl;
+    cin >> my_name;
+    cout<<"입력된 나의 이름은 "<< my_name <<"입니다.";
+    return 0;
+}
+
+
+
+// int main()
+// {
+    
+//     int x =10;
+//     std::cout<<"Hello World"<< std::endl;
+//     std::cout<< 10 << std::endl;
+//     std::cout<< 10.5 << std::endl;
+//     std::cout<< 10.3f << std::endl;
+    
+//     std::cout<< "변수에 저장된 값은 = "<<x; 
+
+//     return 0;
+// }
